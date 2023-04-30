44053aae08ad8e08c51c7111be7a51c2f0c2deff
Jintao Koong <jintaokoong99@gmail.com>
2023-04-30 17:06:48 +0800

    feat: implement first example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { animated, useSpring } from "@react-spring/web";
+
 function App() {
-  return <div>Hello World!</div>;
+  const springs = useSpring({
+    from: { x: 0 },
+    to: { x: 100 },
+  });
+
+  return (
+    <animated.div
+      style={{
+        width: 80,
+        height: 80,
+        backgroundColor: "hotpink",
+        borderRadius: 8,
+        ...springs,
+      }}
+    />
+  );
 }
 
 export default App;
